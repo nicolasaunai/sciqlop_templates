f7ee7efbb7cdfacc272fbb21479d172b4e44ef00
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2023-06-02 15:55:27 +0200

    th

--- themis.py
+++ themis.py
@@ -13,32 +13,32 @@ p.time_range = TimeRange(
 # Spectro ions
 p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peif_en_eflux")
 
+# Bulk ion speed GSE
+p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_MOM/tha_peim_velocity_gsm")
+p.plots[1].plot(
+    "speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peir_t3speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peib_velocity_gse"
+)  # burst
+
 # Tempréature ions
 p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peir_magt3")
-p.plots[1].plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peim_t3_mag")
+p.plots[2].plot("speasy/cda/THEMIS/THA/L2/THA_L2_MOM/tha_peim_t3_mag")
 
 # Champ magnétique GSM
 p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_FGM/tha_fgh_gsm")
-p.plots[2].plot("speasy/cda/THEMIS/THA/L2/THA_L2_FGM/tha_fgs_gsm")
+p.plots[3].plot("speasy/cda/THEMIS/THA/L2/THA_L2_FGM/tha_fgs_gsm")
 
 
 # Density ions
 p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peir_density")
-p.plots[3].plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peib_density")  # burst
-
-# Bulk ion speed GSE
-p.plot("speasy/cda/THEMIS/THA/L2/THA_L2_MOM/tha_peim_velocity_gsm")
-p.plots[4].plot(
-    "speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peir_t3speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peib_velocity_gse"
-)  # burst
+p.plots[4].plot("speasy/cda/THEMIS/THA/L2/THA_L2_ESA/tha_peib_density")  # burst
 
 
-p.plots[1].yAxis.setRange(10, 2000)
-p.plots[2].yAxis.setRange(-75, 75)
-p.plots[3].yAxis.setRange(0.01, 150)
-p.plots[4].yAxis.setRange(-450, 450)
-p.plots[1].yAxis.setScaleType(QCPAxis.stLogarithmic)
-p.plots[3].yAxis.setScaleType(QCPAxis.stLogarithmic)
+p.plots[2].yAxis.setRange(10, 2000)
+p.plots[3].yAxis.setRange(-75, 75)
+p.plots[4].yAxis.setRange(0.01, 150)
+p.plots[1].yAxis.setRange(-450, 450)
+p.plots[4].yAxis.setScaleType(QCPAxis.stLogarithmic)
+p.plots[2].yAxis.setScaleType(QCPAxis.stLogarithmic)
 
 for plot in p.plots:
     plot.replot()
